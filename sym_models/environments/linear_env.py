39932aeae31f8297341153d3753678a089538059
Mohit-coder-droid <mohitsahuandmohitsahu@gmail.com>
2025-08-07 16:29:07 +0530

    Added custom Graph Neural Network feature extractor

--- sym_models/environments/linear_env.py
+++ sym_models/environments/linear_env.py
@@ -6,6 +6,13 @@ import numpy as np
 from stable_baselines3.common.callbacks import BaseCallback
 import mlflow
 from collections import Counter
+import torch
+import torch.nn.functional as F
+from torch.nn import Linear, BatchNorm1d, ModuleList
+from torch_geometric.nn import TransformerConv, TopKPooling
+from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
+from torch_geometric.data import Data, Batch
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
 class ActionStateCallback(BaseCallback):
     """
@@ -143,24 +150,108 @@ class EpisodeEndCallback(BaseCallback):
 
         else:
             print("No active MLflow run found - skipping MLflow logging.")
+
+class GNN(BaseFeaturesExtractor):
+    def __init__(self,observation_space, model_params, features_dim: int = 256):
+        super().__init__(observation_space,features_dim)
+        embedding_size = model_params["model_embedding_size"]
+        n_heads = model_params["model_attention_heads"]
+        self.n_layers = model_params["model_layers"]
+        dropout_rate = model_params["model_dropout_rate"]
+        # top_k_ratio = model_params["model_top_k_ratio"]
+        self.top_k_every_n = model_params["model_top_k_every_n"]
+        # dense_neurons = model_params["model_dense_neurons"]
+        edge_dim = model_params["model_edge_dim"]
+        self.global_node = model_params["global_node"]   # whether to have a global node or not
+
+        feature_size = observation_space['nodes'].shape[1]
+
+        self.conv_layers = ModuleList([])
+        self.transf_layers = ModuleList([])
+        self.bn_layers = ModuleList([])
+
+        # Transformer layers (Convolving by making multi head attention, which has to be concatenated together, so size self.conv1 = (feature_size, n_heads*embedding_size))
+        self.conv1 = TransformerConv(feature_size, embedding_size, heads=n_heads, dropout=dropout_rate, edge_dim=edge_dim, beta=True)
+
+        self.transf1 = Linear(embedding_size*n_heads, embedding_size)
+        self.bn1 = BatchNorm1d(embedding_size)
+
+        # Other layers
+        for i in range(self.n_layers):
+            self.conv_layers.append(TransformerConv(embedding_size,
+                                                    embedding_size,
+                                                    heads=n_heads,
+                                                    dropout=dropout_rate,
+                                                    edge_dim=edge_dim,
+                                                    beta=True))
+
+            self.transf_layers.append(Linear(embedding_size*n_heads, embedding_size))
+            self.bn_layers.append(BatchNorm1d(embedding_size))  # do batch normalization
+
+            # Linear layers
+        self.linear1 = Linear(2*embedding_size, features_dim)
+
+    def _forward(self, x, edge_index, edge_attr, batch_index) -> torch.tensor:
+        # Initial transformation
+        x = self.conv1(x, edge_index)
+        x = torch.relu(self.transf1(x))
+        x = self.bn1(x)
+
+        # Holds the intermediate graph representations
+        global_representation = []
+
+        for i in range(self.n_layers):
+            x = self.conv_layers[i](x, edge_index)
+            x = torch.relu(self.transf_layers[i](x))
+            x = self.bn_layers[i](x)
+            
+            if self.global_node:
+                global_representation.append(torch.cat([gmp(x, batch_index), gap(x, batch_index)], dim=1))
+
+        if self.global_node:
+            x = sum(global_representation)
+
+        # Output block
+        x = torch.relu(self.linear1(x))
+        return x
+
+    def forward(self, observation):
+        # Do batching of the data in pytorch_geometric's way
+        data_list = []
+
+        for i in range(observation["nodes"].size(0)):  # For each graph in the batch
+            x = observation["nodes"][i]                # (7, 3)
+            edge_attr = observation['edge_attributes'][i]   # (6, 1)
+            edge_index = observation['edge_links'][i].to(torch.int64).T  # (2, 6), transpose from (6, 2)
+
+            data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
+            data_list.append(data)
+
+        batch = Batch.from_data_list(data_list)
+
+        return self._forward(batch.x, batch.edge_index, batch.edge_attr, batch.batch)
     
 class LinearSymbolicEnv(gym.Env):
-    def __init__(self, seed=None):
+    def __init__(self,reward_params, seed=None):
         super().__init__()
+        self.reward_params  =reward_params
 
         # Every environment will automatically generate an equation for its own
-        eq = smu.linear_eq(1,seed=seed)
+        # Now let's train it over all the linear environments 
+        self.type = 5
+        eq = smu.linear_eq(self.type,seed=seed)
 
         self.eq = eq
         self.original_eq = eq
 
-        MAX_NODES = 7
-        MAX_EDGES = 6
+        MAX_NODES = 25    # 7 for simple linear equation
+        MAX_EDGES = 24
 
         self.observation_space = spaces.Dict(
             {
                 "nodes": spaces.Box(low=0, high=85, shape=(MAX_NODES, 3), dtype=np.float32),
-                "edge_links": spaces.Box(low=0, high=MAX_NODES-1, shape=(MAX_EDGES, 2), dtype=np.int64),
+                "edge_links": spaces.Box(low=0, high=1, shape=(MAX_EDGES, 2), dtype=np.int64),
+                "edge_attributes":spaces.Box(low=0,high=0, shape=(MAX_EDGES,1),dtype=np.int32)
             },
             seed=seed
         )
@@ -173,9 +264,10 @@ class LinearSymbolicEnv(gym.Env):
 
     def _get_obs(self, eq):
         tree = smu.traverse_expr(eq, returnTreeForPlot=True)
-        edge_index = np.array([list(tree[i][:2]) for i in range(len(tree)) if i!=0])
-        nodes = np.array([node_feature(a[2]) for a in tree])
+        edge_index = np.array([list(tree[i][:2]) for i in range(len(tree)) if i!=0], dtype=np.int64)
+        nodes = np.array([node_feature(a[2]) for a in tree], dtype=np.float32)
 
+        # No need for padding I think
         # Pad nodes and edges to the max size defined in the observation space.
         padded_nodes = np.zeros(self.observation_space["nodes"].shape, dtype=np.float32)
         padded_nodes[:len(nodes)] = nodes
@@ -187,13 +279,15 @@ class LinearSymbolicEnv(gym.Env):
         return {
             "nodes": padded_nodes,
             "edge_links": padded_edge_links,
+            "edge_attributes": np.zeros((len(padded_edge_links), 1))
         }
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
 
         # Every time an environment will be reset it will come up with totally new equation
-        self.eq = smu.linear_eq(1,seed=seed)  
+        self.eq = smu.linear_eq(self.type,seed=seed)
+        self.total_steps = 0
 
         # just to check whether our training is going good or not, let's just play with one equation only, and let the model mug up how to solve that
         # self.eq = self.original_eq
@@ -201,30 +295,77 @@ class LinearSymbolicEnv(gym.Env):
         return self._get_obs(self.eq), {"eq": Main.string(self.eq)}
 
     def step(self, action):
-        TRUNCATION_REWARD = -1.5
-        COMPLITION_REWARD = 10
-        ABSURD_ACTION_REWARD = -0.1
-        NOT_COMPLETION_REWARD = -1  # said that it's completed but it wasn't
-        STEP_REWARD = -0.03
+        TRUNCATION_REWARD = self.reward_params['TRUNCATION_REWARD']
+        COMPLETION_REWARD = self.reward_params['COMPLETION_REWARD']
+        ABSURD_ACTION_REWARD = self.reward_params['ABSURD_ACTION_REWARD']
+        NOT_COMPLETION_REWARD = self.reward_params['NOT_COMPLETION_REWARD']  # said that it's completed but it wasn't
+        STEP_REWARD = self.reward_params['STEP_REWARD']
         terminated, truncated = False, False   # handle the truncated part later on
 
         # Firstly modify the equation, and then see whether model is saying that our equation has been terminated or not
         # punish the model on taking wrong actions
+        reward = 0
 
 
-        new_eq = smu.linear_transport(self.eq, int(action[0]+1), int(action[1]+1))  # check if the model takes some absurd action than what should be done
-        reward = -0.5 if Main.string(self.eq)==Main.string(new_eq) else 0
+
+
+
+
+
+
+
+
+        # Check why this error is coming
+#         RuntimeError: <PyCall.jlwrap (in a Julia function called from Python)
+# JULIA: MethodError: no method matching /(::Nothing, ::SymbolicUtils.BasicSymbolic{Real})
+# The function `/` exists, but no method is defined for this combination of argument types.
+
+# Closest candidates are:
+#   /(!Matched::MutableArithmetics.Zero, ::Any)
+#    @ MutableArithmetics ~/.julia/packages/MutableArithmetics/tNSBd/src/rewrite.jl:74
+#   /(::Any, !Matched::ChainRulesCore.NotImplemented)
+#    @ ChainRulesCore ~/.julia/packages/ChainRulesCore/XAgYn/src/tangent_types/notimplemented.jl:43
+#   /(!Matched::PyObject, ::Any)
+#    @ PyCall ~/.julia/packages/PyCall/1gn3u/src/pyoperators.jl:13
+#   ...
+
+# Stacktrace:
+#   [1] linear_transport(expr_tree::Vector{Tuple{Function, AbstractVector{Any}}}, side::Int64, node::Int64)
+#     @ SymbolicModelsUtils ~/zMohit/RL Models/SymbolicModelsUtils.jl/src/Equation model/linear_model.jl:27
+#   [2] linear_transport(expr::Symbolics.Equation, side::Int64, node::Int64)
+#     @ SymbolicModelsUtils ~/zMohit/RL Models/SymbolicModelsUtils.jl/src/Equation model/linear_model.jl:64
+#   [3] invokelatest(::Any, ::Any, ::Vararg{Any}; kwargs::@Kwargs{})
+#     @ Base ./essentials.jl:1055
+
+        try:
+            new_eq = smu.linear_transport(self.eq, int(action[0]+1), int(action[1]+1))
+        except:
+            new_eq = self.eq
+        
+        # if the size of nodes get increased from the max size, that means the model has done stupid to reach at this point, don't change the equation after this
+        tree = smu.traverse_expr(new_eq, returnTreeForPlot=True)
+        if len(tree)>self.observation_space["nodes"].shape[0]:
+            new_eq = self.eq
+        
+        
+         # check if the model takes some absurd action than what should be done
+        reward = ABSURD_ACTION_REWARD if Main.string(self.eq)==Main.string(new_eq) else 0
+
+
         self.eq = new_eq
 
         if (action[2]==1):  # model is done
             terminated = smu.linear_termination_status(self.eq)
             if (terminated):
-                reward = COMPLITION_REWARD
+                reward = COMPLETION_REWARD
             else:
                 reward = NOT_COMPLETION_REWARD
 
+
+        # This reward is the main reason for the slow training
         self.total_steps += 1
-        # reward += self.total_steps * STEP_REWARD
+        reward += self.total_steps * STEP_REWARD
+        
         if (self.total_steps > self.truncating_step):
             truncated = True
             reward += TRUNCATION_REWARD
